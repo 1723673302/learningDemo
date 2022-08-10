9b68ea69b658eb8f589de67562e979cc0d74a27e
张天宝 <ztianb@foxmail.com>
2022-08-10 22:38:16 +0800

    HttpTool.py :网页请求
    Signed-off-by: 张天宝 <ztianb@foxmail.com>

--- HttpTool.py
+++ HttpTool.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# @Time    : 2022/7/7 14:02
+# @Time    : 2022/8/9 10:52
 # @Author  : 张天宝
 # @Email   : boss@xbtit.cn
-# @File    : HttpTool.py
-# @Project : 上号器
-# @FilePath: E:/pycharmWorkSpace/mytools/上号器\HttpTool.py
+# @File    : temp.py
+# @Project : learningDemo
+# @FilePath: E:/pycharmWorkSpace/github/learningDemo\temp.py
 
-import json
+
 import urllib.error
 import urllib.parse
 import urllib.request
@@ -17,22 +17,49 @@ import mylog
 log = mylog.MyLog()
 
 
-def get_http(url):
+def make_request(url, data):
     try:
-        # 记录一条日志
-        log.debug(f'get_http: {url}')
-
-        response = urllib.request.urlopen(url)
-        html = response.read()
-        remote_data = json.loads(html)
-        if remote_data is None:
-            log.debug(f'get_http: {url} is None')
-            return None
-        if remote_data['code'] is not 200:
-            log.debug(f'get_http: {url} code is not 200')
-            return None
-        log.debug(f'get_http: {url} success')
-        return remote_data
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
+        }
+        log.debug(f'make_request: {url}')
+        log.debug(f'make_request: {data}')
+        request = urllib.request.Request(url, headers=headers, data=data)
     except Exception as e:
         log.error(e)
         return None
+    return request
+
+
+# 发送post请求
+def post_http(request):
+    try:
+        response = urllib.request.urlopen(request)
+        # html_content = response.readlines()
+        # 使用read()读取整个页面内容，使用decode('utf-8')对获取的内容进行编码
+        # print(response.read().decode('utf-8'))
+        # print(response.status)  # 状态码，判断是否成功,200
+        # print(response.getheaders())  # 响应头 得到的一个元组组成的列表
+        # print(response.getheader('Server'))  # 得到特定的响应头
+        if response.status == 200:
+            log.debug(f'post_http: {request.get_full_url()} success')
+            return response
+        # elif response.status == 404:
+
+        else:
+            log.warning(f'post_http: {request.get_full_url()} {response.status}')
+            return None
+    except Exception as e:
+        log.error(f'post_http: {request.get_full_url()} error')
+        log.error(f'{e}')
+        pass
+    # 判断请求是否成功
+
+
+if __name__ == '__main__':
+    # post_http(make_request('http://www.baidu.com', b'123'))
+    # post_http(make_request('https://www.xbgit.cn/daohang/', None))
+    # post_http(make_request('https://httpbin.org/status/200', None))
+    post_http(make_request('https://httpbin.org/status/302', None))
+    # post_http(make_request('https://httpbin.org/status/404', None))
+    # post_http(make_request('https://httpbin.org/status/502', None))
